0ff5df7ba68c677b22af9e3a000bc2129afe3500
HTFDT <almetov_tag@mail.ru>
2023-05-08 14:05:44 +0500

    improved attack controller

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
@@ -12,10 +13,18 @@ public class AttackController : MonoBehaviour
     public Transform firePoint;
     public GameObject projectilePrefab;
     public GameObject projectileContainer;
-    public List<AttackTypeData> attackTypes; 
+    public List<AttackTypeManager> attackTypes;
     private Dictionary<Key, AttackTypeManager> _keyToAttackType;
     private AttackTypeManager _currentAttackType;
     private Coroutine _currentCoroutine;
+
+    [Serializable]
+    public class AttackTypeManager
+    {
+        public List<ModifierCount> modifiers;
+        public List<ProjectileData> projectiles;
+        public float attackDelay;
+    }
 
 
     private void Awake()
@@ -25,25 +34,41 @@ public class AttackController : MonoBehaviour
         actions.Player.SwitchAttackType.performed += SwitchAttackType;
         _keyToAttackType = new Dictionary<Key, AttackTypeManager>
         {
-            [Key.Q] = new(attackTypes[0]),
-            [Key.W] = new(attackTypes[1]),
-            [Key.E] = new(attackTypes[2]),
-            [Key.R] = new(attackTypes[3])
+            [Key.Q] = attackTypes[0],
+            [Key.W] = attackTypes[1],
+            [Key.E] = attackTypes[2],
+            [Key.R] = attackTypes[3]
         };
         _currentAttackType = _keyToAttackType[Key.Q];
     }
 
     private void Start()
     {
-        _currentCoroutine = StartCoroutine(_currentAttackType.LaunchCoroutine(firePoint, projectilePrefab, projectileContainer,
-            _currentAttackType.Projectiles, _currentAttackType.AttackDelay, _currentAttackType.Modifiers));
+        _currentCoroutine = StartCoroutine(LaunchAttackCycle());
     }
 
     private void SwitchAttackType(InputAction.CallbackContext context)
     {
         _currentAttackType = _keyToAttackType[(context.control as KeyControl)!.keyCode];
         StopCoroutine(_currentCoroutine);
-        _currentCoroutine = StartCoroutine(_currentAttackType.LaunchCoroutine(firePoint, projectilePrefab, projectileContainer,
-            _currentAttackType.Projectiles, _currentAttackType.AttackDelay, _currentAttackType.Modifiers));
+        _currentCoroutine = StartCoroutine(LaunchAttackCycle());
+    }
+
+    public IEnumerator LaunchAttackCycle()
+    {
+        while (true)
+        {
+            foreach (var data in _currentAttackType.projectiles)
+            {
+                var proj = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation,
+                    projectileContainer.transform);
+                proj.GetComponent<Projectile>().Init(data);
+                foreach (var mod in _currentAttackType.modifiers.Concat(data.BaseModifiers)
+                             .OrderBy(mod => (mod.modifier.Tag, mod.modifier.weight)))
+                    mod.modifier.ApplyTo(proj, mod.count);
+            }
+
+            yield return new WaitForSeconds(_currentAttackType.attackDelay);
+        }
     }
 }
