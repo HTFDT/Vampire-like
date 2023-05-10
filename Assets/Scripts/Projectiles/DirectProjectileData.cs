728541777687506eba1d8280f3954b114c0ffa70
HTFDT <almetov_tag@mail.ru>
2023-05-10 15:13:58 +0500

    added homing projectile

--- Assets/Scripts/Projectiles/DirectProjectileData.cs
+++ Assets/Scripts/Projectiles/DirectProjectileData.cs
@@ -5,18 +5,11 @@
 public class DirectProjectileData : ProjectileData
 {
     [SerializeField] private float speed;
-    
-    public override void StartActions(Rigidbody2D rb, ChainNode<Rigidbody2D> next)
-    {
-        base.StartActions(rb, next);
-        rb.freezeRotation = true;
-    }
 
     public override void UpdateActions(Rigidbody2D rb, ChainNode<Rigidbody2D> next)
     {
         base.UpdateActions(rb, next);
         var transform = rb.transform;
-        transform.position += transform.right * speed * Time.fixedDeltaTime;
-        next?.Action.Invoke(rb, next.Next);
+        transform.position += transform.right * (speed * Time.fixedDeltaTime);
     }
 }
