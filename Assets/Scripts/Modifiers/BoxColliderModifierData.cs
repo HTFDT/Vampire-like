9373f0951bb74f0f015e21bfc32c3256a850e277
Vasili188 <vasyokulgin@gmail.com>
2023-05-13 02:00:13 +0500

    added earth spike projectile and earth attack

---
+++ Assets/Scripts/Modifiers/BoxColliderModifierData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Modifiers/New BoxColliderModifierData")]
+public class BoxColliderModifierData : ModifierData
+{
+    public override ModifierTag Tag => ModifierTag.OnUpdateAction;
+
+    public override void ApplyTo(GameObject projectile, int modifierCount)
+    {
+        projectile.AddComponent<BoxCollider2D>();
+        projectile.GetComponent<Projectile>().UpdateActions.AddLast(KeepColliderForm);
+    }
+
+    private void KeepColliderForm(Rigidbody2D rb, ChainNode<Rigidbody2D> next)
+    {
+        var collider = rb.gameObject.GetComponent<BoxCollider2D>();
+        var spriteRenderer = rb.gameObject.GetComponent<SpriteRenderer>();
+        collider.size = spriteRenderer.size;
+        next?.Action.Invoke(rb, next.Next);
+    }
+}
---
+++ Assets/Scripts/Projectiles/SelfInstantiatingProjectileData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Projectiles/New SelfInstantiatingProjectile")]
+public class SelfInstantiatingProjectileData : ProjectileData
+{
+    public GameObject projectilePrefab;
+    public float delayBeforeNextSegmentSpawn;
+
+    public override void StartActions(Rigidbody2D rb, ChainNode<Rigidbody2D> next)
+    {
+        base.StartActions(rb, next);
+        var camProps = Camera.main!.GetComponent<CameraProps>();
+        var script = rb.gameObject.GetComponent<Projectile>();
+        if (rb.position.x < camProps.Left || rb.position.x > camProps.Right) return;
+        script.StartCoroutine(SpawnNextSegment(rb, script));
+    }
+
+    private IEnumerator SpawnNextSegment(Rigidbody2D rb, Projectile script)
+    {
+        yield return new WaitForSeconds(delayBeforeNextSegmentSpawn);
+        var transform = rb.transform;
+        var spriteRenderer = rb.gameObject.GetComponent<SpriteRenderer>();
+        var proj = Instantiate(projectilePrefab, rb.position + (Vector2)(transform.right * (spriteRenderer.size.x * .5f)),
+            transform.rotation);
+        proj.SetActive(false);
+        var sc = proj.GetComponent<Projectile>();
+        sc.rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        sc.Init(script.data, script.AdditionalModifiers);
+        proj.SetActive(true);
+    }
+}
