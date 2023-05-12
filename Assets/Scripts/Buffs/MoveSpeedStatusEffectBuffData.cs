ba33c128cabf03533d8acea685e784479f72eda2
Vasili188 <vasyokulgin@gmail.com>
2023-05-12 21:07:49 +0500

    added move speed reduce status effect debuff and modifier

---
+++ Assets/Scripts/Buffs/MoveSpeedStatusEffectBuffData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Buffs
+{
+    [CreateAssetMenu(menuName = "Buffs/New MoveSpeedStatusEffectBuffData")]
+    public class MoveSpeedStatusEffectBuffData : BuffData
+    {
+        public float decrement;
+        public Color spriteColor;
+        public override IEnumerator Buff(GameObject obj, Func<bool> predicate, Action onDurationEnd)
+        {
+            var script = obj.GetComponent<Enemy>();
+            var spriteRenderer = obj.GetComponent<SpriteRenderer>();
+            var initialColor = spriteRenderer.color;
+            spriteRenderer.color = spriteColor;
+            script.moveSpeed -= script.moveSpeed * decrement;
+            yield return new WaitWhile(predicate);
+            script.moveSpeed = script.initialMoveSpeed;
+            spriteRenderer.color = initialColor;
+            onDurationEnd();
+        }
+    }
+}
