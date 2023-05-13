55305b9c842201d91e65e1484ea3f3f20b0ce8e1
Vasili188 <vasyokulgin@gmail.com>
2023-05-13 22:09:36 +0500

    added destroy out of camera projectiles modifier

--- Assets/Scripts/Modifiers/DestroyAfterOutOfCameraModifierData.cs
+++ Assets/Scripts/Modifiers/DestroyAfterOutOfCameraModifierData.cs
@@ -5,6 +5,7 @@
 public class DestroyAfterOutOfCameraModifierData : ModifierData
 {
     public override ModifierTag Tag => ModifierTag.OnUpdateAction;
+    public float distanceFromCameraBorder;
 
     public override void ApplyTo(GameObject projectile, int modifierCount)
     {
@@ -12,7 +13,10 @@ public class DestroyAfterOutOfCameraModifierData : ModifierData
 
         void DestroyIfOutOfCamera(Rigidbody2D rb, ChainNode<Rigidbody2D> next)
         {
-            if (!cameraProps.InBounds(rb.position))
+            if (rb.position.y > cameraProps.Top + distanceFromCameraBorder || 
+                rb.position.y < cameraProps.Bottom - distanceFromCameraBorder ||
+                rb.position.x > cameraProps.Right + distanceFromCameraBorder ||
+                rb.position.x < cameraProps.Left - distanceFromCameraBorder)
                 Destroy(rb.gameObject);
             next?.Action(rb, next.Next);
         }
