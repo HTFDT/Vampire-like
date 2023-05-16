9be738a14fba9170bd8275a69215587e9916153d
Vasili188 <vasyokulgin@gmail.com>
2023-05-16 16:32:49 +0500

    added air to fire synergy and fire explosion projectile

---
+++ Assets/Scripts/Modifiers/ProjectileSynergyModifierData.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Modifiers/New ProjectileSynergyModifierData")]
+public class ProjectileSynergyModifierData : SynergyModifierData
+{
+    public ProjectileData projectileToSpawn;
+    public GameObject projectilePrefab;
+    
+    public override void ApplyTo(GameObject projectile, int modifierCount)
+    {
+        var script = projectile.GetComponent<Projectile>();
+        var projectileContainer = GameObject.FindWithTag("ProjectileContainer").transform;
+        script.OnCollisionActions.AddLast(SpawnProjectileOnCollision);
+
+        void SpawnProjectileOnCollision(Collision2D col, ChainNode<Collision2D> next)
+        {
+            next?.Action(col, next.Next);
+            if (!col.gameObject.CompareTag("Enemy")) return;
+            var cont = col.gameObject.GetComponent<BuffContainer>();
+            if (cont == null) return;
+            if (cont.BuffDataToDuration.Keys.All(buff => buff.attackType != withAttackType)) return;
+            var contact = col.GetContact(0);
+            var proj = Instantiate(projectilePrefab, contact.point, script.transform.rotation, projectileContainer);
+            proj.SetActive(false);
+            proj.GetComponent<Projectile>().Init(projectileToSpawn, Enumerable.Empty<ModifierCount>());
+            proj.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectiles/ExplosionProjectileData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Projectiles/New ExplosionProjectileData")]
+public class ExplosionProjectileData : ProjectileData
+{
+    public override void StartActions(Rigidbody2D rb, ChainNode<Rigidbody2D> next)
+    {
+        base.StartActions(rb, next);
+        rb.transform.rotation = Quaternion.identity;
+    }
+}
